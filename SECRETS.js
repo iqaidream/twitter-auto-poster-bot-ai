3df31ac4e78b6781cde3ff8c13cfe2c7ae3fd5ee
iqaidream <iqai.dream@gmail.com>
2025-08-20 16:40:15 +0200

    Update SECRETS.js

--- SECRETS.js
+++ SECRETS.js
@@ -1,8 +1,8 @@
 //? It is advisable to use environment variables instead of directly putting secrets in repository file but I have skipped this part as it would become complicated for many.
 //? Alternatively you can download the repository instead or forking and upload it from your account and keep it private, in that way, your secrets will not be exposed to the public.
 
-const APP_KEY = "ZEg3RTBtb1hua0Rka3lGUkVLRlc6MTpjaQ";
-const APP_SECRET = "rrRQSPYW5z6AEtHZnqUvuDo2mIUkNNGS37gKuLOCn4ur-J1nnE";
+const APP_KEY = "pdH3UseWKQ5adDAzBQprWXfzk";
+const APP_SECRET = "zww2YB8096v4w4sXO96m47lkFU9FRu7GLev3p9Ek42gPHcZ75k";
 const ACCESS_TOKEN = "1958148247924645888-kuxi7vJOXgouE0YjdmYqBduGQuDGM7";
 const ACCESS_SECRET = "7W0IQ1qrECj2fvnq2SwaA0KBiEF0ruCkl8xEPSuVjOLVd";
 const GEMINI_API_KEY = "AIzaSyAJuz_j1-VUUXRYLFiPn0fT_ayr37LfqWI";
